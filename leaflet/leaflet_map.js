c40a46bb6a6dc3a636be59cbb90458fb7e292206
Raghav Aggarwal <raghavaggsss@gmail.com>
2019-06-19 15:01:14 -0700

    layer selection ayer per year for gbif. grayscale map. trigger for sei.

--- leaflet/leaflet_map.js
+++ leaflet/leaflet_map.js
@@ -26,6 +26,45 @@ function countKeys(obj) {
     return Object.keys(obj).length;
 }
 
+
+var highlight = {
+    'color': '#ffffff',
+    'weight': 2,
+    'opacity': 1,
+    'fillColor': '#ffffff',
+    'fillOpacity': 0.5
+};
+
+var remove_highlight = {
+    'color': "#d2d2d2",
+    'weight': 1,
+    'fillColor': "#8d8c91",
+    'fillOpacity': 1
+};
+
+
+var typeEco = {
+    "ME": "#F012BE",
+    "SE": "#85144b",
+    "XX": "rgb(39,39,48)",
+    "YS": "#c9c731"
+};
+
+var typeSE = {
+    "OF": "#2ECC40",
+    "MF": "#3D9970",
+    "WD": "#653f2e",
+    "RI": "#00236e",
+    "IT": "#eeaf8c",
+    "WN": "#0074D9",
+    "HB": "#98Fb98",
+    "SV": "#857e4e",
+    "ES": "#7FDBFF",
+    "FW": "#39CCCC",
+    "AP": "#AAAAAA",
+
+};
+
 // initialize the map
 
 
@@ -151,48 +190,115 @@ var dotIcon = L.icon({
 });
 
 
+var shapes_layer;
+$.getJSON("SEI.topojson", function (data) {
+    shapes_layer = L.topoJson(data, {
+
+        style: function (feature) {
+            sstyle = remove_highlight;
+            if (feature.properties.SE_ME_1 != "SE") {
+                sstyle.fillColor = typeEco[feature.properties.SE_ME_1];
+            } else {
+                sstyle.fillColor = typeSE[feature.properties.SECl_1];
+            }
+
+            return sstyle;
+        },
+        onEachFeature: function (feature, layer) {
+            var popUpInfo = feature.properties.Comp1Lgnd_;
+            if (feature.properties.Comp2Lgnd_) {
+                popUpInfo += "<br/>" + feature.properties.Comp2Lgnd_;
+                if (feature.properties.Comp3Lgnd_) {
+                    popUpInfo += "<br/>" + feature.properties.Comp3Lgnd_;
+                }
+            }
+            popUpInfo += "</br> Quality: " + feature.properties.QualityNo_ + "/5.0";
+            if (feature.properties.Location) {
+                popUpInfo += "</br> Location: " + feature.properties.Location;
+            }
+            // var popUp = layer.bindPopup(popUpInfo);
+            // popUp.on('popupclose', function() {
+            //     // layer.setStyle(remove_highlight);
+            // });
+            layer.on('click', function () {
+                layer.bringToFront();
+                if (layer.selected) {
+                    sstyle = remove_highlight;
+                    if (feature.properties.SE_ME_1 != "SE") {
+                        sstyle.fillColor = typeEco[feature.properties.SE_ME_1];
+                    } else {
+                        sstyle.fillColor = typeSE[feature.properties.SECl_1];
+                    }
+                    layer.setStyle(sstyle);
+                    layer.selected = 0;
+                    layer.unbindPopup();
+                } else {
+                    sstyle = highlight;
+                    // if (feature.properties.SE_ME_1 != "SE") {
+                    //     sstyle.fillColor = typeEco[feature.properties.SE_ME_1];
+                    // }
+                    // else {
+                    //     sstyle.fillColor = typeSE[feature.properties.SECl_1];
+                    // }
+                    layer.setStyle(sstyle);
+                    layer.selected = 1;
+                    layer.bindPopup(popUpInfo).openPopup();
+                }
+            })
+        }
+    })
+        .addTo(map);
+    overlays.SEI = shapes_layer;
+    if (countKeys(overlays) == num_overlays) {
+        L.control.layers(baseLayers, overlays, {collapsed: false}).addTo(map);
+    }
+});
+
+
 
 var points_layer_options = {
     pointToLayer: function (feature, latlng) {
         var marker = L.marker(latlng, {icon: dotIcon});
+        if (feature.properties) {
+            var redList = feature.properties.redList;
+            var markerPopUp = '<div class="popUpfeature">' + feature.properties.species + '<br/> <b>'
+                + feature.properties.common.split(',')[0] + '</b>';
+            if (redList) {
+                markerPopUp += '<b class="redListFont">' + '<br/>' + feature.properties.redList + '</b>';
+            }
+
+            markerPopUp += '<br/>';
+
+            var id_species = feature.properties.species;
+            if (id_species) {
+                id_species = id_species.replace(/\s+/g, '_');
+            }
+
+            // markerPopUp += '<input type="button" value="wikiImg" '+  ' onClick="loadImg(\'' + feature.properties.species + '\')" />';
+
+            // markerPopUp += '<input type="button" value="Wiki" '+ ' onClick="goToWiki(\'' + feature.properties.species + '\')" />';
+
+            markerPopUp += '<input type="button" class="wikiButton" value="Loading" id=' + (id_species + "_wiki") + '>';
+
+            markerPopUp += '<div class="thumbnail"  id=' + (id_species + "_thumb") + '> </div>';
+
+            markerPopUp += '</div>';
+
+            marker.bindPopup(markerPopUp);
+
+            marker.on('click', function () {
+                loadImg(feature.properties.species);
+            });
+        }
         // var marker = L.marker(latlng, {title: ""});
-        var redList = feature.properties.redList;
-        console.log(feature.properties);
-        var markerPopUp = '<div class="popUpfeature">' + feature.properties.species + '<br/> <b>'
-            + feature.properties.common.split(',')[0] + '</b>';
-        if (redList) {
-            markerPopUp += '<b class="redListFont">' + '<br/>' + feature.properties.redList + '</b>';
-        }
-
-        markerPopUp += '<br/>';
-
-        var id_species = feature.properties.species;
-        if (id_species) {
-            id_species = id_species.replace(/\s+/g, '_');
-        }
-
-        // markerPopUp += '<input type="button" value="wikiImg" '+  ' onClick="loadImg(\'' + feature.properties.species + '\')" />';
-
-        // markerPopUp += '<input type="button" value="Wiki" '+ ' onClick="goToWiki(\'' + feature.properties.species + '\')" />';
-
-        markerPopUp += '<input type="button" class="wikiButton" value="Loading" id=' + (id_species + "_wiki") + '>';
-
-        markerPopUp += '<div class="thumbnail"  id=' + (id_species + "_thumb") + '> </div>';
-
-        markerPopUp += '</div>';
-
-        marker.bindPopup(markerPopUp);
-
-        marker.on('click', function () {
-            loadImg(feature.properties.species);
-        });
-
         return marker;
     }
 };
 
-var points_layer = L.geoJson({ "type": "FeatureCollection", "features": [{ "type": "Feature", "properties": { "gbifID": 255736378, "datasetKey": "4fa7b334-ce0d-4e88-aaae-2e0c138d049e", "occurrenceID": "URN:catalog:CLO:EBIRD_CAN:OBS81006245", "kingdom": "Animalia", "phylum": "Chordata", "class": "Aves", "order": "Charadriiformes", "family": "Laridae", "genus": "Larus", "species": "Larus glaucescens", "taxonRank": "SPECIES", "scientificName": "Larus glaucescens J.F.Naumann, 1840", "countryCode": "CA", "locality": "Vancouver--Spanish Banks", "publishingOrgKey": "e2e717bf-551a-4917-bdc9-4fa0f342c530", "decimalLatitude": 49.277941, "decimalLongitude": -123.22897, "eventDate": "2010-01-06T00:00:00Z", "day": 6.0, "month": 1.0, "year": 2010.0, "taxonKey": 2481196, "speciesKey": 2481196.0, "basisOfRecord": "HUMAN_OBSERVATION", "institutionCode": "CLO", "collectionCode": "EBIRD_CAN", "catalogNumber": "OBS81006245", "license": "CC0_1_0", "recordedBy": "obsr38830", "lastInterpreted": "2019-03-29T01:15:37.476Z", "issue": "COORDINATE_ROUNDED" }, "geometry": { "type": "Point", "coordinates": [ -123.22897, 49.277941 ] } },] },
-                        );
+// var points_layer = L.geoJson({ "type": "FeatureCollection", "features": [{ "type": "Feature", "properties": { "Unnamed: 0": 70300, "datasetKey": "50c9509d-22c7-4a22-a47d-8c48425ef4a7", "kingdom": "Plantae", "phylum": "Tracheophyta", "class": "Magnoliopsida", "order": "Asterales", "family": "Asteraceae", "genus": "Tanacetum", "species": "Tanacetum vulgare", "decimalLatitude": 49.174106, "decimalLongitude": -123.120081, "eventDate": "2019-04-26T20:44:53Z", "day": 26.0, "month": 4.0, "year": 2019.0, "redList": null, "common": "common tansy, tansy, golden-buttons, garden tansy" }, "geometry": { "type": "Point", "coordinates": [ -123.120081, 49.174106 ] } },]},
+//                        points_layer_options );
+
+// var points_layer = L.geoJSON(null, points_layer_options);
 
 var clusters = L.markerClusterGroup(
             {
@@ -220,7 +326,7 @@ var clusters = L.markerClusterGroup(
                 }, singleMarkerMode: 1
             }
         );
-        clusters.addLayer(points_layer);
+clusters.addTo(map);
         // var clusters_layer = map.addLayer(clusters);
         // overlays.Gbif = clusters;
         // if (countKeys(overlays) == num_overlays) {
@@ -229,197 +335,82 @@ var clusters = L.markerClusterGroup(
 
 gbif_parts_filenames = ["UBC2010.geojson", "UBC2014.geojson", "UBC2018.geojson"];
 gbif_years = [];
-var init_year = 2000;
-for (init_year; init_year <= 2019; init_year++) {
-    gbif_years.push(init_year);
-}
+points_layers = [];
+var gbif_json_sent = [];
 
-map.addLayer(clusters);
-
-function loadPointsJson(filename) {
-    $.getJSON(filename, function (data) {
-
+function loadPointsJson(year) {
+    if (!gbif_json_sent[year]) {
+        $.getJSON("gbif/"+ year.toString() + ".geojson", function (data) {
         // add GeoJSON layer to the map once the file is loaded
-        points_layer.addData(data);
-        clusters.addLayer(points_layer);
+
+        points_layers[year].addData(data);
+        clusters.addLayer(points_layers[year]);
+        // points_layer.addData(data);
+
+        // clusters.removeLayer(points_layer);
+            gbif_json_sent[year] = 1
     });
+    }
+    else {
+        clusters.addLayer(points_layers[year]);
+    }
+
 }
 
-gbif_years.forEach(function(file) {
-    loadPointsJson("gbif/"+ file.toString() + ".geojson");
-});
+function deletePointsLayer(year) {
+    clusters.removeLayer(points_layers[year]);
+}
 
-// gbif_parts_filenames.forEach(function(file) {
-//     loadPointsJson("gbif_parts/"+ file);
+var year_button_div = document.getElementById("year-buttons");
+
+var init_year = 1990;
+
+for (let i = init_year; i <= 2019; i++) {
+    gbif_json_sent[i] = 0;
+    gbif_years.push(i);
+    points_layers[i] = L.geoJSON(null, points_layer_options);
+    year_button = document.createElement("input");
+    year_button.setAttribute("type", "checkbox");
+    year_button.id = "year-" + i.toString();
+    year_button.onclick = function() {
+        if (this.checked) {
+             loadPointsJson(this.id.split('-')[1])
+        }
+        else {
+            deletePointsLayer(this.id.split('-')[1]);
+        }
+
+    };
+
+    year_label = document.createElement("label");
+    year_label.setAttribute("for", year_button.id);
+    year_label.innerText = i.toString();
+
+
+    year_button_div.appendChild(year_button);
+    year_button_div.appendChild(year_label);
+}
+//
+// map.addLayer(clusters);
+
+
+
+// gbif_years.forEach(function(year) {
+//     loadPointsJson("gbif/"+ year.toString() + ".geojson", year);
 // });
 
 
-// $.getJSON("red.geojson",function(data){
-//     var redStar = L.icon({
-//     iconUrl: 'red_star.png',
-//     iconSize: [20,20]
-//   });
-//   L.geoJson(data  ,{
-//     pointToLayer: function(feature,latlng){
-// 	  return L.marker(latlng,{icon: redStar, riseOnHover: 1});
-//     }
-//   }  ).addTo(map);
-// });
-
-var highlight = {
-    'color': '#ffffff',
-    'weight': 2,
-    'opacity': 1,
-    'fillColor': '#ffffff',
-    'fillOpacity': 0.5
-};
-
-var remove_highlight = {
-    'color': "#d2d2d2",
-    'weight': 1,
-    'fillColor': "#8d8c91",
-    'fillOpacity': 1
-};
 
 
-var typeEco = {
-    "ME": "#F012BE",
-    "SE": "#85144b",
-    "XX": "rgb(39,39,48)",
-    "YS": "#c9c731"
-};
 
-var typeSE = {
-    "OF": "#2ECC40",
-    "MF": "#3D9970",
-    "WD": "#653f2e",
-    "RI": "#00236e",
-    "IT": "#eeaf8c",
-    "WN": "#0074D9",
-    "HB": "#98Fb98",
-    "SV": "#857e4e",
-    "ES": "#7FDBFF",
-    "FW": "#39CCCC",
-    "AP": "#AAAAAA",
 
-};
+
+
+
+
 
 // "#FF851B", "#01FF70","#FFDC00" ,"#F012BE",
 
-// var shapes_layer;
-// $.getJSON("SEI.topojson", function (data) {
-//     shapes_layer = L.topoJson(data, {
-//
-//         style: function (feature) {
-//             sstyle = remove_highlight;
-//             if (feature.properties.SE_ME_1 != "SE") {
-//                 sstyle.fillColor = typeEco[feature.properties.SE_ME_1];
-//             } else {
-//                 sstyle.fillColor = typeSE[feature.properties.SECl_1];
-//             }
-//
-//             return sstyle;
-//         },
-//         onEachFeature: function (feature, layer) {
-//             var popUpInfo = feature.properties.Comp1Lgnd_;
-//             if (feature.properties.Comp2Lgnd_) {
-//                 popUpInfo += "<br/>" + feature.properties.Comp2Lgnd_;
-//                 if (feature.properties.Comp3Lgnd_) {
-//                     popUpInfo += "<br/>" + feature.properties.Comp3Lgnd_;
-//                 }
-//             }
-//             popUpInfo += "</br> Quality: " + feature.properties.QualityNo_ + "/5.0";
-//             if (feature.properties.Location) {
-//                 popUpInfo += "</br> Location: " + feature.properties.Location;
-//             }
-//             // var popUp = layer.bindPopup(popUpInfo);
-//             // popUp.on('popupclose', function() {
-//             //     // layer.setStyle(remove_highlight);
-//             // });
-//             layer.on('click', function () {
-//                 layer.bringToFront();
-//                 if (layer.selected) {
-//                     sstyle = remove_highlight;
-//                     if (feature.properties.SE_ME_1 != "SE") {
-//                         sstyle.fillColor = typeEco[feature.properties.SE_ME_1];
-//                     } else {
-//                         sstyle.fillColor = typeSE[feature.properties.SECl_1];
-//                     }
-//                     layer.setStyle(sstyle);
-//                     layer.selected = 0;
-//                     layer.unbindPopup();
-//                 } else {
-//                     sstyle = highlight;
-//                     // if (feature.properties.SE_ME_1 != "SE") {
-//                     //     sstyle.fillColor = typeEco[feature.properties.SE_ME_1];
-//                     // }
-//                     // else {
-//                     //     sstyle.fillColor = typeSE[feature.properties.SECl_1];
-//                     // }
-//                     layer.setStyle(sstyle);
-//                     layer.selected = 1;
-//                     layer.bindPopup(popUpInfo).openPopup();
-//                 }
-//             })
-//         }
-//     })
-//         .addTo(map);
-//     overlays.SEI = shapes_layer;
-//     if (countKeys(overlays) == num_overlays) {
-//         L.control.layers(baseLayers, overlays, {collapsed: false}).addTo(map);
-//     }
-// });
 
 
-// var htmlLegend1and2 = L.control.htmllegend({
-//         position: 'bottomright',
-//         legends: [{
-//             name: 'Layer 1',
-//             layer: points_layer,
-//             elements: [{
-//                 label: 'Rectangle',
-//                 html: '',
-//                 style: {
-//                     'background-color': 'red',
-//                     'width': '10px',
-//                     'height': '10px'
-//                 }
-//             }, {
-//                 label: 'Triangle',
-//                 html: '',
-//                 style: {
-//                     'background-color': 'green',
-//                     'width': '10px',
-//                     'height': '10px'
-//                 }
-//             }]
-//         }],
-//         collapseSimple: true,
-//         detectStretched: true,
-//         collapsedOnInit: true,
-//         defaultOpacity: 0.7,
-//         visibleIcon: 'icon icon-eye',
-//         hiddenIcon: 'icon icon-eye-slash'
-//     });
-//     map.addControl(htmlLegend1and2);
-//
-//
-// var DeleteControl  = L.Control.extend({
-//         options: {
-//             position: 'topright'
-//         },
-//         onAdd: function(m) {
-//             var btn = L.DomUtil.create('button')
-//             btn.innerText = 'Delete layer 1'
-//             L.DomEvent.on(btn, 'click', function() {
-//                 // Remove legend uses entry id, which can be accessed through `htmllegend._entries`
-//                 htmlLegend1and2.removeLegend(1)
-//                 m.removeControl(this)
-//             })
-//             return btn
-//         }
-//     })
-//     map.addControl(new DeleteControl())
 
-//
-//     L.control.layers(null, overlays, { collapsed: false }).addTo(map);
