25ec27ec8dad8b93839ebe43cf8c12a3167c9f6e
Gabe <gksmith@psych.ubc.ca>
2019-07-12 12:21:47 -0700

    relative freq bug

--- ShinyMap/server.R
+++ ShinyMap/server.R
@@ -1,13 +1,19 @@
 server <- function(input,output, session){
   output$plot1 = renderPlot({
-    x = df2 %>%
+    raw = df2 %>%
       filter(species %in% input$species, year>=input$year[1], year<=input$year[2]) %>%
       group_by(species, year) %>%
       tally() %>%
       drop_na() %>%
       add_zeros()
-    y <- yearly_obs[which(yearly_obs$year %in% x$year),]
-    x <- x %>% mutate(normalized = x$n/y$n)
+    x = NULL
+    for (sp in unique(raw$species)) {
+      sdf = raw[which(raw$species==sp),]
+      sdf = sdf[order(sdf$year),]
+      y = yearly_obs[which(yearly_obs$year %in% sdf$year),]
+      sdf <- sdf %>% mutate(normalized = sdf$n/y$n)
+      x = rbind(x, sdf)
+    }
     
     plot_normalize <- if (nrow(x) > 1) {
       ggplot(x, aes(x=year, y=normalized, color = species)) + 
@@ -23,8 +29,8 @@ server <- function(input,output, session){
       obsr_seq = (x$n-1):(x$n+1)
       ggplot(x, aes(x=year, y=normalized, color = species)) + 
         geom_point() + 
-        scale_x_discrete(breaks = year_seq, limits = year_seq) +
-        scale_y_discrete(breaks = obsr_seq, limits = obsr_seq) +
+        scale_x_continuous(breaks = year_seq, limits = year_seq) +
+        scale_y_continuous(breaks = obsr_seq, limits = obsr_seq) +
         coord_cartesian(xlim = year_seq, ylim = obsr_seq) +
         labs(title = "Reported Species Occurence Over Time",
              x = "Year",
@@ -34,8 +40,7 @@ server <- function(input,output, session){
     
     
     plot_raw <- if (nrow(x) > 1) {
-      year_seq = (x$year-1):(x$year+1)
-      obsr_seq = (x$n-1):(x$n+1)ggplot(x, aes(x=year, y=n, color = species)) + 
+      ggplot(x, aes(x=year, y=n, color = species)) + 
         geom_line() + 
         geom_point() + 
         scale_x_continuous() + 
@@ -48,8 +53,8 @@ server <- function(input,output, session){
       obsr_seq = (x$n-1):(x$n+1)
       ggplot(x, aes(x=year, y=n, color = species)) + 
         geom_point() + 
-        scale_x_discrete(breaks = year_seq, limits = year_seq) +
-        scale_y_discrete(breaks = obsr_seq, limits = obsr_seq) +
+        scale_x_continuous(breaks = year_seq, limits = year_seq) +
+        scale_y_continuous(breaks = obsr_seq, limits = obsr_seq) +
         coord_cartesian(xlim = year_seq, ylim = obsr_seq) +
         labs(title = "Reported Species Occurence Over Time",
              x = "Year",
