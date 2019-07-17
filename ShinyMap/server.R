2c78d297321ffcc98d5fbd75d12d17031490e4ac
Lesley <lesley.miller@alumni.ubc.ca>
2019-07-17 14:26:10 -0700

    fixed Y axis on time series plot

--- ShinyMap/server.R
+++ ShinyMap/server.R
@@ -40,15 +40,18 @@ server <- function(input,output, session){
               axis.title.y = element_text(face = "bold", size = 14),
               axis.text = element_text(face = "bold", size = 14),
               legend.title = element_text(face = "bold", size = 12),
-              legend.text = element_text(size = 12))
+              legend.text = element_text(size = 12))+
+        expand_limits(y = 0)
+      
+      
     }else {
       year_seq = (x$year-1):(x$year+1)
-      obsr_seq = (x$n-1):(x$n+1)
+      obsr_seq = 0:(2*x$normalized)
       ggplot(x, aes(x=year, y=normalized, color = species)) + 
         geom_point() + 
         scale_x_continuous(breaks = year_seq, limits = year_seq) +
         scale_y_continuous(breaks = obsr_seq, limits = obsr_seq) +
-        coord_cartesian(xlim = year_seq, ylim = obsr_seq) +
+        coord_cartesian(xlim = year_seq, ylim = obsr_seq, expand = T) +
         labs(title = "Reported Species Occurence Over Time",
              x = "Year",
              y = "Proportion of Total Species Observations"
@@ -60,7 +63,7 @@ server <- function(input,output, session){
               legend.title = element_text(face = "bold", size = 12),
               legend.text = element_text(size = 12))
     }
-    
+  
     
     plot_raw <- if (nrow(x) > 1) {
       ggplot(x, aes(x=year, y=n, color = species)) + 
@@ -76,14 +79,17 @@ server <- function(input,output, session){
               axis.title.y = element_text(face = "bold", size = 14),
               axis.text = element_text(face = "bold", size = 14),
               legend.title = element_text(face = "bold", size = 12),
-              legend.text = element_text(size = 12))
+              legend.text = element_text(size = 12))+
+        expand_limits(y = 0)
+      
     }else {
       year_seq = (x$year-1):(x$year+1)
-      obsr_seq = (x$n-1):(x$n+1)
+      #obsr_seq = (x$n-1):(x$n+1)
+      obsr_seq = 0:(x$n+1)
       ggplot(x, aes(x=year, y=n, color = species)) + 
         geom_point() + 
-        scale_x_continuous(breaks = year_seq, limits = year_seq) +
-        scale_y_continuous(breaks = obsr_seq, limits = obsr_seq) +
+        scale_x_continuous(breaks = year_seq) +
+        scale_y_continuous(breaks = obsr_seq) +
         coord_cartesian(xlim = year_seq, ylim = obsr_seq) +
         labs(title = "Reported Species Occurence Over Time",
              x = "Year",
