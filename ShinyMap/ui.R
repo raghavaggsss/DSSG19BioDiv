4ed6d2b78f3acc8d64b201a0f059440dcd645a47
Lesley <lesley.miller@alumni.ubc.ca>
2019-07-26 17:04:37 -0700

    added explanation text to shiny app

--- ShinyMap/ui.R
+++ ShinyMap/ui.R
@@ -21,10 +21,16 @@ ui <- fluidPage(
                   multiple = T),
       radioButtons(inputId = "normalization",
                    label = "Normalization Options",
-                   choices = c("Raw Counts", "Proportion of Total Observations", "Proportion of Kingdom Observations", "Proportion of Class Observations"))),
+                   choices = c("Raw Counts", "Proportion of Total Observations", "Proportion of Kingdom Observations", "Proportion of Class Observations")),
+      wellPanel(
+        tags$small(paste0(
+          "Note: Raw Counts is the number of species records for a given year. Normalizing by Total Observations in a given year corrects for the fact that GBIF occurrence records are increasing every year, regardless whether a given species is actually increasing in abundance. Normalizing by Kingdom or Class corrects for certain organisms being oversampled in certain years compared to others."))
+      )),
     
 
     mainPanel(
+      h5("The time series graph below visualizes species occurrence records from the GBIF database for the area selected on the map. You may customize the plot by adjusting the year interval, taxon level (ie genus, species etc.) or choose a custom grouping like pollinators or endangered species. Normalization options are provided to correct for biases when comparing records across years."),
+      headerPanel(""),
       plotOutput("plot1")
     )
   )
