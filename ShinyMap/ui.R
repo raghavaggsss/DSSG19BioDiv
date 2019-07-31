abdd618852d07f247f07c0b8e2910fb97f73e89d
Lesley <lesley.miller@alumni.ubc.ca>
2019-07-31 13:49:50 -0700

    updated shiny UI

--- ShinyMap/ui.R
+++ ShinyMap/ui.R
@@ -37,20 +37,21 @@ ui <- fluidPage(
                 trigger = "hover"),
       
       bsTooltip(id = "member",
-                title = "Choose a sub-grouping of the main Category chosen above. IUCN, are species listed as globally endangered by the International Union for Conservation of Nature. SARA are endangered species protected through the federal Canadian Species At Risk Act. BC Red and BC Blue are provincially endangered species. BC Endemic are species that are found only in BC",
+                title = "Choose a sub-grouping of the main Category chosen above. IUCN, are species listed as globally endangered by the International Union for Conservation of Nature. SARA are endangered species protected through the federal Canadian Species At Risk Act. BC Red and BC Blue are provincially endangered species. BC Endemic are species that are found only in BC.",
                 placement = "bottom",
                 trigger = "hover"),
       
       bsTooltip(id = "normalization",
                 title = "Note: Raw Counts is the number of species records for a given year. Normalizing by Total Observations in a given year corrects for the fact that GBIF occurrence records are increasing every year, regardless whether a given species is actually increasing in abundance. Normalizing by Kingdom or Class corrects for certain organisms being oversampled in certain years compared to others.",
                 placement = "bottom",
-                trigger = "hover")),
+                trigger = "hover")
+      ),
     
 
     mainPanel(
       bsPopover(id = "plot1",
                 title = "How to Interpret this Plot",
-                content = "The time series graph above visualizes species occurrence records for the area selected on the map. Normalization options in the panel to the left are provided to correct for biases when comparing records across years.",
+                content = "The time series graph above visualizes species occurrence records for the area selected on the map. The y-axis is measuring the abundance of species records and is assumed to only approximate actual species abundance. Normalization options in the panel to the left are provided to correct for biases when comparing records across years.",
                 placement = "bottom",
                 trigger = "hover"),
       plotOutput("plot1")
